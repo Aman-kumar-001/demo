4e02f29cdbaf088c37d61839e5dcf596787f287c
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2023-04-16 02:17:20 +0530

    setting up the banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
 import Navbar from "../src/component/Navbar";
+import Banner from "../src/component/Banner/banner";
+
 
 function App() {
   return (
+    <>
    <Navbar />
+   <Banner />
+   </>
   );
-}
+};
 
 export default App;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,7 +4,7 @@ import {GiHamburgerMenu } from "react-icons/gi";
 import "./Navbar.css";
 
 const Navbar = () => {
-    // const [ShowMediaIcons, setShowMediaIcons] = useState(false);
+    const [ShowMediaIcons, setShowMediaIcons] = useState(false);
   return (
      <nav className='main-container'>
         <div className='logo'>
@@ -33,8 +33,26 @@ const Navbar = () => {
         <div className='right'>
          <a href='#' ><FaSignInAlt />Login</a>
 
-        <div className='menu' >
+        <div className='menu' onClick={() => setShowMediaIcons(!ShowMediaIcons)} >
           <GiHamburgerMenu />
+          <div className={ShowMediaIcons ? console.log(true) :console.log(false)}>
+            <div className='mobile-menu'>
+          <ul>
+                <li>
+                    <a href=''>Home</a>
+                </li>
+                <li>
+                    <a href=''>contact</a>
+                </li>
+                <li>
+                    <a href=''>category</a>
+                </li>
+                <li>
+                    <a href=''>About</a> 
+                </li>
+            </ul>
+            </div>
+          </div>
          </div>
         </div>
      </nav>
