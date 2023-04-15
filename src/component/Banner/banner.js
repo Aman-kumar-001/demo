abfa332ceca735f153d0ea58d0491051cea2d494
Aman kumar <104027397+Aman-kumar-001@users.noreply.github.com>
2023-04-16 03:53:04 +0530

    till banner

--- src/component/Banner/banner.js
+++ src/component/Banner/banner.js
@@ -1,11 +1,40 @@
 import React from 'react'
 import "./banner.css";
+import Mycard from "../Banner/image-item/mycard";
+import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai";
+
 
 
 function banner() {
-  return (
-    
-  )
-}
+    let box = document.querySelector('.product-container'); 
+    const prebtn = () => {
+      let width= box.clientWidth;
+      box.scrollLeft = box.scrollLeft - width;
+    } 
+    const nextbtn = () => {
+        let width= box.clientWidth;
+        box.scrollLeft = box.scrollLeft + width;
+    } 
+      return (
+    <div className='product-carousel'>
+        <button className='pre-btn' onClick={prebtn}><p><AiOutlineArrowLeft /></p></button>
+        <button className='next-btn' onClick={nextbtn}><p><AiOutlineArrowRight/></p></button>
+
+        <div className='product-container'>
+            < Mycard cardno= '1'/>
+            < Mycard cardno= '2'/>
+            < Mycard cardno= '3'/>
+            < Mycard cardno= '4'/>
+            < Mycard cardno= '5'/>
+            < Mycard cardno= '6'/>
+            < Mycard cardno= '7'/>
+            < Mycard cardno= '8'/>
+            < Mycard cardno= '9'/>
+            < Mycard cardno= '10'/>
+        </div>
+
+    </div>
+  );
+};
 
 export default banner;
---
+++ src/component/Banner/image-item/mycard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../image-item/mycard.css";
+
+
+export const mycard = (props) => {
+  return (
+    <div className='mycard'>mycard no. {props.cardno}</div>
+   
+  );
+};
+
+export default mycard;
